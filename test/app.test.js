8c5614b0e919e73862a873e58e1a6b5c580836a9
Joseba Legarreta <josebalega@gmail.com>
2014-12-27 16:07:25 +0100

    Load req.validator hook in the tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,19 +1,18 @@
-/**
-* Module dependencies
-*/
-
+// Module dependencies
 var Sails = require('sails');
 
 // Load logic from `config/routes` directory
 var routes = require('./server/config/routes');
+// Load reqValidator hook
+var reqValidator = require('../index');
 
 before(function(done) {
   Sails.lift({
     // Configuration for testing purposes
     port: 1337,
     log: {level: process.env.log || 'warn'},
-    hooks: { grunt: false },
-    globals: true,
+    hooks: { grunt: false, validator: reqValidator },
+    globals: {async: false, services: false, models: false, _: false},
     routes: routes
   }, function(err, sails) {
     // Start the tests
